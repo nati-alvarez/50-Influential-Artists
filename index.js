d285d38c2a1fa2096291041091eea69e7f19f1c7
Natividad Alvarez <tivialvarez@gmail.com>
2020-05-01 20:20:49 -0400

    Completed JS tasks 2 and 3

--- index.js
+++ index.js
@@ -215,7 +215,8 @@ console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh";
+console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -225,9 +226,12 @@ console.log(artists[2].bio);
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
-  }
+function getArtistByIndex(artists, id) {
+    let name = artists[id].name;
+    return `The artist at index ${id} is ${name}`;
+}
+
+console.log(getArtistByIndex(artists, 0))
   
   /**
 
